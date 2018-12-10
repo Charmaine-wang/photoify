d4164df150e5e8b646c99b29db1af233aa3db1ec
charmaine wang <charmainewang424@hotmail.com>
2018-12-10 12:46:42 +0100

    made folders to the project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+
+  <form class="account" action="index.html" method="post">
+
+    <input type="text" name="fname" placeholder="Firstname">
+    <input type="text" name="lname" placeholder="Lastname">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="password" placeholder="Password">
+  </form>
+
+</body>
+</html>
